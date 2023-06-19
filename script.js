ada185beda515c4ae930472f319f7df51b8942cb
KyleS115 <Kyle.Smith115@gmail.com>
2023-06-19 16:03:57 -0400

    Added interactable buttons and display to the game

--- script.js
+++ script.js
@@ -5,84 +5,86 @@ function getComputerChoice() {
     if that number is <= 1/3, returns "Rock"
     if that number is > 1/3 and <= 2/3, returns "Paper"
     if that number is > 2/3, returns "Scissors" */
-    computerNumber = Math.random()
+    computerNumber = Math.random();
     if (computerNumber <= 1 / 3) {
-        computerChoice = "Rock"
+        computerChoice = "Rock";
     } else if (computerNumber <= 2 / 3) {
-        computerChoice = "Paper"
+        computerChoice = "Paper";
     } else {
-        computerChoice = "Scissors"
-    }
-    return (computerChoice)
-}
+        computerChoice = "Scissors";
+    };
+    return (computerChoice);
+};
 
-function playRound() {
+function playRound(playerChoice) {
     /* Plays a single round of rock paper scissors
 
-    Prompts and the saves the users's choice in lowercase
     Retrieves and saves the computer's choice in lowercase
     If the choices are the same, returns a string for the tie scenario
-    If the player wins, returns a string for the win scenario
-    If the player loses, returns a string for the loss scenario */
-    playerChoice = prompt("Rock, paper, or scissors?").toLowerCase()
-    computerChoice = getComputerChoice().toLowerCase()
+    If the player wins, returns a string for the win scenario, increases round number and player score
+    If the player loses, returns a string for the loss scenario, increases round number and computer score */
+    computerChoice = getComputerChoice().toLowerCase();
     if (playerChoice === computerChoice) {
-        return "It's a tie! You chose " + playerChoice + " and I chose " + computerChoice + "."
+        return "It's a tie! You chose " + playerChoice + " and I chose " + computerChoice + ".";
     } else if ((playerChoice === "rock" && computerChoice === "scissors") || (playerChoice === "paper" && computerChoice === "rock") || (playerChoice === "scissors" && computerChoice === "paper")) {
-        return "You win! You chose " + playerChoice + " and I chose " + computerChoice + "."
+        round++
+        playerScore++
+        return "You win this round. You chose " + playerChoice + " and I chose " + computerChoice + ".";
     } else {
-        return "You lose! You chose " + playerChoice + " and I chose " + computerChoice + "."
-    }
-}
+        round++
+        computerScore++
+        return "You lose this round. You chose " + playerChoice + " and I chose " + computerChoice + ".";
+    };
+};
 
-function game() {
-    /* Plays a 5 round game of rock paper scissors
+let round = 0
+let playerScore = 0
+let computerScore = 0
 
-    Sets both the player and computer score at 0
-    Sets the current round to 1 
-    Logs the win condition
-    For every round <= 5,
-        Logs the round number
-        Plays and saves the outcome of a single round
-        Logs the round's outcome
-        If the player wins,
-            Increases the player's score by 1
-            Logs the current scores
-            Increases the round number var by 1
-        If the computer wins,
-            Increases the computer's score by 1
-            Logs the current scores
-            Increases the round number var by 1
-        If theres a tie,
-            Logs that the round doesn't count
-            The round number var remains unchanged
-    Logs the final outcome of the game*/
-    let playerScore = 0
-    let computerScore = 0
-    let currentRound = 1
-    console.log("First to five wins!")
-    for (;currentRound <= 5; currentRound++) {
-        console.log("Round: " + currentRound)
-        roundOutcome = playRound()
-        console.log(roundOutcome)
-        if (roundOutcome.slice(0, 8) === "You win!") {
-            playerScore++
-            console.log("Your Score: " + playerScore)
-            console.log("Computer's Score: " + computerScore)
-        } else if (roundOutcome.slice(0, 9) === "You lose!") {
-            computerScore++
-            console.log("Your Score: " + playerScore)
-            console.log("Computer's Score: " + computerScore)
-        } else {
-            console.log("Let's try that again...")
-            currentRound--
+const rockButton = document.getElementById('rock');
+const paperButton = document.getElementById('paper');
+const scissorsButton = document.getElementById('scissors');
+console.log(rockButton)
+
+const roundResults = document.getElementById('roundresults');
+const gameResults = document.getElementById('gameresults');
+const roundDisplay = document.getElementById('round');
+const playerScoreDisplay = document.getElementById('playerscore');
+const computerScoreDisplay = document.getElementById('computerscore');
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        if (round <= 4) {
+            rockButton.style.border = '8px inset white';
+            rockButton.style.backgroundColor = 'lightgrey'
+            paperButton.style.border = '8px inset white';
+            paperButton.style.backgroundColor = 'lightgrey'
+            scissorsButton.style.border = '8px inset white';
+            scissorsButton.style.backgroundColor = 'lightgrey'
+            result = playRound(button.innerText.toLowerCase());
+            roundResults.innerHTML = result;
+            roundDisplay.innerHTML = 'ROUND: ' + round;
+            playerScoreDisplay.innerHTML = 'PLAYER: ' + playerScore;
+            computerScoreDisplay.innerHTML = 'COMPUTER: ' + computerScore;
+            if (result.slice(0, 7) === 'You win') {
+                button.style.border = '8px inset green';
+                button.style.backgroundColor = 'lightgreen';
+            } else if (result.slice(0, 8) === 'You lose') {
+                button.style.border = '8px inset red';
+                button.style.backgroundColor = 'pink';
+            } else {
+                button.style.border = '8px inset yellow';
+                button.style.backgroundColor = 'lightyellow';
+            }
         }
-    }
-    if (playerScore > computerScore) {
-        console.log("Congratulatons! You've won!")
-    } else {
-        console.log("Better luck next time...")
-    }
-}
-
-game()
+        if (round === 5) {
+            if (playerScore > computerScore) {
+                gameResults.innerHTML = "Congratulations! You've won!"
+            } else {
+                gameResults.innerHTML = "Better luck next time!"
+            }
+        }
+        return;
+    });
+});
